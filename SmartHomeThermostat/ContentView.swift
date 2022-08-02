08d503c23da47269afae1f8e34ab262c2ee2a287
Josa Vicente <jvicenpe@gmail.com>
2022-08-02 21:49:37 +0200

    Initial Commit

---
+++ SmartHomeThermostat/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SmartHomeThermostat
+//
+//  Created by Josafat Vicente Pérez on 2/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmartHomeThermostat/SmartHomeThermostatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeThermostatApp.swift
+//  SmartHomeThermostat
+//
+//  Created by Josafat Vicente Pérez on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeThermostatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
